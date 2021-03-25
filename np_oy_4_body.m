660f8695dc4c8507e5f90e9df18ac4f3b83e11f2
Tim Li <77031993+tl355@users.noreply.github.com>
2021-03-25 09:20:34 -0600

    Converted all matrices to sparse matrices

--- np_oy_4_body.m
+++ np_oy_4_body.m
@@ -1,54 +1,53 @@
 x = [0 1 ; 1 0]; y = [0 -1i ; 1i 0]; z = [1 0 ; 0 -1];
 
 J = 1;
-x_113  = kron(kron(eye(2^0),x),eye(2^13));
-z_113  = kron(kron(eye(2^0),z),eye(2^13));
+x_113  = sparse(kron(kron(eye(2^0),x),eye(2^13)));
+z_113  = sparse(kron(kron(eye(2^0),z),eye(2^13)));
 
-x_122  = kron(kron(eye(2^1),x),eye(2^12));
-z_122  = kron(kron(eye(2^1),z),eye(2^12));
+x_122  = sparse(kron(kron(eye(2^1),x),eye(2^12)));
+z_122  = sparse(kron(kron(eye(2^1),z),eye(2^12)));
 
-x_114  = kron(kron(eye(2^2),x),eye(2^11));
-z_114  = kron(kron(eye(2^2),z),eye(2^11));
+x_114  = sparse(kron(kron(eye(2^2),x),eye(2^11)));
+z_114  = sparse(kron(kron(eye(2^2),z),eye(2^11)));
 
-x_124  = kron(kron(eye(2^3),x),eye(2^10));
+x_124  = sparse(kron(kron(eye(2^3),x),eye(2^10)));
 
-x_211  = kron(kron(eye(2^4),x),eye(2^9));
-z_211  = kron(kron(eye(2^4),z),eye(2^9));
+x_211  = sparse(kron(kron(eye(2^4),x),eye(2^9)));
+z_211  = sparse(kron(kron(eye(2^4),z),eye(2^9)));
 
-x_221  = kron(kron(eye(2^5),x),eye(2^8));
+x_221  = sparse(kron(kron(eye(2^5),x),eye(2^8)));
 
-x_213  = kron(kron(eye(2^6),x),eye(2^7));
+x_213  = sparse(kron(kron(eye(2^6),x),eye(2^7)));
 
-x_222  = kron(kron(eye(2^7),x),eye(2^6));
+x_222  = sparse(kron(kron(eye(2^7),x),eye(2^6)));
 
-z_111  = kron(kron(eye(2^8),z),eye(2^5));
+z_111  = sparse(kron(kron(eye(2^8),z),eye(2^5)));
 
-z_112  = kron(kron(eye(2^9),z),eye(2^4));
+z_112  = sparse(kron(kron(eye(2^9),z),eye(2^4)));
 
-z_014  = kron(kron(eye(2^10),z),eye(2^3));
+z_014  = sparse(kron(kron(eye(2^10),z),eye(2^3)));
 
-z_103  = kron(kron(eye(2^11),z),eye(2^2));
+z_103  = sparse(kron(kron(eye(2^11),z),eye(2^2)));
 
+x_a111 = sparse(kron(kron(eye(2^12),x),eye(2^1)));
+y_a111 = sparse(kron(kron(eye(2^12),y),eye(2^1)));
+z_a111 = sparse(kron(kron(eye(2^12),z),eye(2^1)));
 
-x_a111 = kron(kron(eye(2^12),x),eye(2^1));
-y_a111 = kron(kron(eye(2^12),y),eye(2^1));
-z_a111 = kron(kron(eye(2^12),z),eye(2^1));
+x_a112 = sparse(kron(kron(eye(2^13),x),eye(2^0)));
+y_a112 = sparse(kron(kron(eye(2^13),y),eye(2^0)));
+z_a112 = sparse(kron(kron(eye(2^13),z),eye(2^0)));
 
-x_a112 = kron(kron(eye(2^13),x),eye(2^0));
-y_a112 = kron(kron(eye(2^13),y),eye(2^0));
-z_a112 = kron(kron(eye(2^13),z),eye(2^0));
-
-I_size = 2^14;
+I = sparse(eye(2^14));
 
 A = 1; U = 1; t = 1;
 
-H_8_body = -J*(x_113*x_122*x_114*x_124*x_211*x_221*x_213*x_222 + z_111*z_112*z_113*z_114 + z_014*z_111 + z_112*z_103 + z_114*z_211 + z_113*z_122);
+H_8_body = sparse(-J*(x_113*x_122*x_114*x_124*x_211*x_221*x_213*x_222 + z_111*z_112*z_113*z_114 + z_014*z_111 + z_112*z_103 + z_114*z_211 + z_113*z_122));
 
-H_4_body = -A*(z_111*z_112*z_113*z_114 + z_014*z_111 + z_112*z_103 + z_114*z_211 + z_113*z_122 ...
-    + (eye(I_size) - z_a111 + z_a112 + z_a111*z_a112) * (z_a111 + z_a112 + z_a111*z_a112 - eye(I_size)) ...
-    + (eye(I_size) + z_a111 - z_a112 + z_a111*z_a112) * (eye(I_size) - z_a111 - z_a112 - z_a111*z_a112)) ...
-    + (U/2)*(z_a111 + z_a112 + z_a111*z_a112 - eye(I_size)) ...
+H_4_body = sparse(-A*(z_111*z_112*z_113*z_114 + z_014*z_111 + z_112*z_103 + z_114*z_211 + z_113*z_122 ...
+    + (I - z_a111 + z_a112 + z_a111*z_a112) * (z_a111 + z_a112 + z_a111*z_a112 - I) ...
+    + (I + z_a111 - z_a112 + z_a111*z_a112) * (I - z_a111 - z_a112 - z_a111*z_a112)) ...
+    + (U/2)*(z_a111 + z_a112 + z_a111*z_a112 - I) ...
     + (t/2)*((x_a112 + z_a111*x_a112)*x_113*x_114 + (x_a111*x_a112 + y_a111*y_a112)*x_122*x_124 ...
-    + (x_a112 - z_a111*x_a112)*x_221*x_222 + (x_a111*x_a112 - y_a111*y_a112)*x_211*x_213 );
+    + (x_a112 - z_a111*x_a112)*x_221*x_222 + (x_a111*x_a112 - y_a111*y_a112)*x_211*x_213 ));
 
 abs(min(eig(H_8_body))-min(eig(H_4_body)))
